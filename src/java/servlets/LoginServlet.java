e2fb06a5521be26c0d3b67b61d4d028fe26e1b8d
786097 <786097@SAIT227594IT.ACDM.DS.SAIT.CA>
2021-06-16 21:05:02 -0600

    First Commit Files created

---
+++ src/java/servlets/LoginServlet.java
@@ -0,0 +1,29 @@
+
+package servlets;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+
+
+public class LoginServlet extends HttpServlet {
+
+    
+
+ 
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+      
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+     
+    }
+
+   
+
+}
---
+++ web/WEB-INF/login.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : login
+    Created on : Jun 16, 2021, 8:58:57 PM
+    Author     : 786097
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+    </body>
+</html>
